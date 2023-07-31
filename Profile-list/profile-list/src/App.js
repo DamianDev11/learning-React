de8b1777a4a9cd01820526f5e747dd53468e1a71
Damian <damianprakash07@gmail.com>
2023-08-01 00:26:36 +0530

    multiple cards rendering

--- Profile-list/profile-list/src/App.js
+++ Profile-list/profile-list/src/App.js
@@ -1,15 +1,39 @@
 import Profile from "./Component/Profile";
 
-const profileDetials = {
-  image: "https://unsplash.it/900/400/",
-  name: "Dam",
-  role: "SDE-1",
-};
+const profileDetials = [
+  {
+    image: "https://unsplash.it/300/300/",
+    name: "Dam1",
+    role: "SDE-1",
+    email: "dam1@gmail.com",
+  },
+  {
+    image: "https://unsplash.it/300/300/",
+    name: "Dam2",
+    role: "SDE-1",
+    email: "dam1@gmail.com",
+  },
+  {
+    image: "https://unsplash.it/300/300/",
+    name: "Dam3",
+    role: "SDE-1",
+    email: "dam1@gmail.com",
+  },
+  {
+    image: "https://unsplash.it/300/300/",
+    name: "Dam4",
+    role: "SDE-1",
+    email: "dam1@gmail.com",
+  },
+];
 
 const App = () => (
-  <div>
+  <div className="list-container">
     <h1>Profile List</h1>
-    <Profile userDetails={profileDetials} />
+    <Profile userDetails={profileDetials[0]} />
+    <Profile userDetails={profileDetials[1]} />
+    <Profile userDetails={profileDetials[2]} />
+    <Profile userDetails={profileDetials[3]} />
   </div>
 );
 
--- Profile-list/profile-list/src/Component/Profile/index.js
+++ Profile-list/profile-list/src/Component/Profile/index.js
@@ -1,12 +1,13 @@
 const Profile = (props) => {
   const { userDetails } = props;
   return (
-    <div profile-card-container>
+    <div className="profile-card-container">
       <img className="profile-pic" src={userDetails.image} alt="profile-pic " />
-      ;
+
       <div>
         <h1 className="profile-name">{userDetails.name}</h1>
         <p className="profile-role">{userDetails.role}</p>
+        <p className="profile-role">{userDetails.email}</p>
       </div>
     </div>
   );
