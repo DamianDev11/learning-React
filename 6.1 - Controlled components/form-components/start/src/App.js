e6d2835f6ceabbb35c3f48dc588bafb87b850f26
Damian <damianprakash07@gmail.com>
2023-08-02 23:26:24 +0530

    uncontrolled form component

---
+++ 6.1 - Controlled components/form-components/start/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MessageBox from "./MessageBox";
+
+const App = () => (
+  <MessageBox
+    nameVal="John Doe"
+    natureVal="Tech Support"
+    queryVal="Problem with my account"
+  />
+);
+
+export default App;
