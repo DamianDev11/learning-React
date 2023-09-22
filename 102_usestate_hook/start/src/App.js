518a1f6cba21b7435b81ae76154e9884c4bfffa1
Damian <damianprakash07@gmail.com>
2023-09-22 22:26:47 +0530

    Introducing useState-functional component

---
+++ 102_usestate_hook/start/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Config from "./components/Config";
+import TagsBuilder from "./components/TagsBuilder";
+
+const App = () => {
+  const [scheduler, setScheduler] = useState(false);
+  const [location, setLocation] = useState(false);
+  const [tags, setTags] = useState([]);
+  return (
+    <div className="container">
+      <Config
+        title="Use Scheduler"
+        toggleState={scheduler}
+        onToggle={(val) => {
+          setScheduler(val);
+        }}
+      />
+      <Config
+        title="Use Location"
+        toggleState={location}
+        onToggle={(val) => {
+          setLocation(val);
+        }}
+      />
+      <TagsBuilder
+        tags={tags}
+        addTags={(value) =>
+          setTags([...tags, { id: new Date().getTime(), title: value }])
+        }
+      />
+    </div>
+  );
+};
+
+export default App;
